9a9ae7cd266c6eb8360763b2b71163927f414b3b
Los-Ciaparitos <los.ciaparitos@rekru.hub.pl>
2024-08-04 16:50:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+(function() {
+    let isDevToolsOpen = false;
+
+    const checkDevTools = function() {
+        const threshold = 160;
+        const widthDifference = window.outerWidth - window.innerWidth;
+        const heightDifference = window.outerHeight - window.innerHeight;
+
+        if (widthDifference > threshold || heightDifference > threshold) {
+            isDevToolsOpen = true;
+            window.location.href = 'https://nohello.net';
+        }
+    };
+
+    window.addEventListener('resize', checkDevTools);
+    setInterval(checkDevTools, 1000); 
+})();
+
+let isFormSubmitted = localStorage.getItem('isFormSubmitted') === 'true';
+
 document.getElementById('recruitment-form').addEventListener('submit', function(event) {
     event.preventDefault();
+
+    if (isFormSubmitted) {
+        alert('Formularz został już wysłany.');
+        return;
+    }
 
     const discordNick = document.getElementById('discord-nick').value;
     const uid = document.getElementById('uid').value;
@@ -47,6 +72,8 @@ document.getElementById('recruitment-form').addEventListener('submit', function(
         if (response.ok) {
             alert('Formularz został wysłany!');
             document.getElementById('recruitment-form').reset();
+            isFormSubmitted = true;
+            localStorage.setItem('isFormSubmitted', 'true');
         } else {
             alert('Wystąpił błąd przy wysyłaniu formularza.');
         }
